f4f45acfec9ab351dd990d085232d3b92c86fd7a
yurchenko08 <yurchenko08@gmail.com>
2022-11-16 19:51:50 +0200

    finished task2

--- client.js
+++ client.js
@@ -1,4 +1,18 @@
-const remoteUrl = 'wss://boiling-beach-26008.herokuapp.com';
-const localUrl = 'ws://localhost:8082';
+const remoteUrl = "wss://boiling-beach-26008.herokuapp.com";
+const localUrl = "ws://localhost:8082";
 
 //Your implementation
+const client = new WebSocket(localUrl);
+client.onopen = () => {
+  console.log("client connected");
+};
+
+const send = () => {
+  let nickname = document.getElementById("username").value;
+  let message = document.getElementById("message").value;
+  client.send(`${nickname}: ${message}`);
+};
+client.onmessage = ({ data }) => {
+  const chat = document.getElementById("chat");
+  chat.innerHTML += `${data}\n`;
+};
--- server/index.js
+++ server/index.js
@@ -1,4 +1,17 @@
 //Your implementation
+const ws = require("ws");
+const wss = new ws.Server({ port: 8082 });
+const clients = [];
+wss.on("connection", (connection) => {
+  clients.push(connection);
+  console.log("new client connected");
 
+  connection.on("message", (message) => {
+    wss.clients.forEach((client) => client.send(message.toString()));
+  });
+  connection.on("close", () => {
+    console.log("client disconnected");
+  });
+});
 // Do not remove this export. wss should be the name of you WebSocket Server instance
 module.exports = wss;
